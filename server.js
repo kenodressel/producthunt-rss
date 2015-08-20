7bf669d15c215a390fe3839a045fc76cb8d55c56
Keno Dressel <ardobras@gmail.com>
2015-08-20 22:46:26 +0200

    added dummy file & dir creation

--- server.js
+++ server.js
@@ -8,20 +8,46 @@ var unirest = require('unirest');
 var rss = require('rss');
 var fs = require('fs');
 var moment = require('moment');
-
 var CronJob = require('cron').CronJob;
-cronRun();
+
+//Run init() on startup
+init();
+
+//Create a cronjob that runs ever 5 minutes.
 new CronJob('0 */5 * * * *', function () {
     cronRun();
 }, null, true, "America/Los_Angeles");
 
 var config = require('./config');
+
 var ph = {
     'access_token': null,
     'expires': null,
     'limit': 50 //Min Upvotes to be published
 };
 
+/**
+ * Creates the directory 'data' if it not exists and runs the inital generation of the rss.xml
+ */
+function init() {
+    if (!fs.existsSync(__dirname + "/data")) {
+        //Create data directory
+        fs.mkdirSync(__dirname + "/data", 0766, function (err) {
+            if (err) console.log(err);
+        });
+
+        //Create dummy posts.json file
+        fs.writeFile(__dirname + '/data/posts.json', '[]');
+    }
+
+    cronRun();
+
+}
+
+/**
+ * Get's an access token from the producthunt API
+ * @param cb
+ */
 
 function auth(cb) {
     unirest.post('https://api.producthunt.com/v1/oauth/token')
@@ -32,11 +58,20 @@ function auth(cb) {
             'grant_type': "client_credentials"
         })
         .end(function (response) {
-            ph.access_token = response.body.access_token;
-            ph.expires = response.body.expires_in * 1000 + Date.now();
-            cb();
+            if (response.body.error) {
+                console.error(response.body.error_description);
+            } else {
+                ph.access_token = response.body.access_token;
+                ph.expires = response.body.expires_in * 1000 + Date.now();
+                cb();
+            }
         });
 }
+
+/**
+ * Querys the current featured posts fro the producthunt API
+ * @param cb
+ */
 
 function getCurrentPosts(cb) {
     if (ph.expires > Date.now()) {
@@ -53,6 +88,10 @@ function getCurrentPosts(cb) {
     }
 }
 
+/**
+ * Reads the saved JSON and compares it to the current available posts
+ * If new posts occure it will regenerate the rss.xml
+ */
 
 function cronRun() {
     fs.readFile(__dirname + '/data/posts.json', function (err, json) {
@@ -85,6 +124,9 @@ function cronRun() {
     });
 }
 
+/**
+ * (re)Renders the RSS from the posts.json
+ */
 
 function renderRSS() {
     /* lets create an rss feed */
@@ -119,8 +161,14 @@ function renderRSS() {
 
     });
 
-
 }
+
+/**
+ * A helper function to comapre if a post is already saved in the file
+ * @param posts
+ * @param id
+ * @returns {boolean}
+ */
 
 function findById(posts, id) {
     for (var i = 0; i < posts.length; i++) {
@@ -130,10 +178,10 @@ function findById(posts, id) {
 }
 
 
+//Catch all requests and return the rss file
 app.get('*', function (req, res) {
     res.sendFile(__dirname + '/data/rss.xml');
 });
 
+//Start the express server
 app.listen(3000);
-
-console.log('Express server started on port 3000');
