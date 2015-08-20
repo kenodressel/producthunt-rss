7c30aa94bf85693953e34296a4bb5b8b253f2922
Keno Dressel <Ardobras@users.noreply.github.com>
2015-08-20 22:21:30 +0200

    Create config-sample.js for reference

---
+++ config-sample.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Ardobras on 20.08.2015.
+ */
+
+//Get you id & secret here: https://api.producthunt.com/v1/oauth/applications
+module.exports = {
+    'client_id': "<YOUR API KEY>",
+    'client_secret': "<YOUR API SECRET>"
+};
